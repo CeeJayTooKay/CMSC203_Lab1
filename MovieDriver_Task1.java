bf5c083cbbdb3369c3d18cb23bab0b546500ee2d
Christopher Johnson <114847216+CeeJayTooKay@users.noreply.github.com>
2023-09-25 21:47:17 -0400

    Add files via upload

---
+++ MovieDriver_Task1.java
@@ -0,0 +1,36 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class MovieDriver_Task1 {
+
+	public static void main(String[] args) {
+//Creates Scanner and Movie instance
+		Scanner scn = new Scanner(System.in);
+		Movie first = new Movie();
+		
+//Requests and gets, and sets the name of the Movie instance
+		System.out.println("What is the name of the movie?");
+		String a = scn.nextLine();
+		first.setTitle(a);
+		
+//Requests and gets, and sets the rating of the Movie instance	
+		System.out.println("What is the movie's rating?");
+		String b = scn.nextLine();
+		first.setRating(b);
+
+//Requests and gets, and sets the amount of tickets sold at the theatre		
+		System.out.println("How many tickets were sold at the theatre?");
+		int c = scn.nextInt();
+		first.setSoldTickets(c);
+		
+//toString print to display the info input by the user
+		System.out.println(first);
+
+//Good bye print to end the program
+		System.out.println("Goodbye");
+
+	scn.close();	
+	} 
+
+}
---
+++ MovieDriver_Task2.java
@@ -0,0 +1,46 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class MovieDriver_Task2 {
+
+	public static void main(String[] args) {
+//String used to help the loop run		
+		String d = "y";
+		
+//Creates Scanner and Movie instance		
+		Scanner scn = new Scanner(System.in);
+		Movie first = new Movie();
+		
+//Loop Starts		
+		while(d.equals("y")){
+			
+//Requests and gets name of the Movie instance			
+		System.out.println("What is the name of the movie?");
+		String a = scn.nextLine();
+		first.setTitle(a);
+//Requests and gets the rating of the Movie instance		
+		System.out.println("What is the movie's rating?");
+		String b = scn.nextLine();
+		first.setRating(b);
+
+//Requests and gets, and sets the amount of tickets sold at the theatre		
+		System.out.println("How many tickets were sold at the theatre?");
+		int c = scn.nextInt();
+		first.setSoldTickets(c);
+
+//toString print to display the info input by the user	
+		System.out.println(first);
+		
+//Asks the user if they want to enter info for another movie and the loop either continues or ends
+		System.out.println("Do you want to enter another? (y or n)");
+		d = scn.next().toLowerCase();
+		scn.nextLine();
+	}
+//Good bye print to end the program	
+		System.out.println("Goodbye");
+		
+	scn.close();
+	} 
+
+}
